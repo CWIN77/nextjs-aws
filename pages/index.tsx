f327ce5c7a34d2b2619c6747457c9b0216a8f0e8
CWIN77 <cwstbp0426@gmail.com>
2022-07-18 15:19:31 +0900

    next-aws practice project

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import styled from 'styled-components'
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      Next<br />Typescript<br />Styled-Components
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width:100vw;
+  min-height:100vh;
+  font-size:18px;
+`
+
+export default Home
