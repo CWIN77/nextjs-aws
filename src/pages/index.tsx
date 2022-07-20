bb2655f08f4e393a93f4f2b4a70e8e425725e456
CWIN77 <cwstbp0426@gmail.com>
2022-07-21 02:42:56 +0900

    add delete add post

---
+++ src/pages/index.tsx
@@ -0,0 +1,109 @@
+import { API, withSSRContext } from 'aws-amplify'
+import type { GetStaticProps, NextPage } from 'next'
+import Link from 'next/link'
+import styled from 'styled-components'
+import { getPost, listPosts } from '../graphql/queries'
+import { createPost as createPostQuery } from "../graphql/mutations"
+import { useState } from 'react'
+import { IPost } from '../types'
+
+const Home: NextPage<{posts:IPost[]}> = ({ posts }: {posts:IPost[]}) => {
+  const [title,setTitle] = useState("");
+  const [descript,setDescript] = useState("");
+
+  const getRandomUid = () => {
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'; // 63
+    let uid = ''
+    for (let i = 0; i < 8; i++) {
+      const randomNum = Math.floor(Math.random() * chars.length)
+      uid += chars.substring(randomNum, randomNum + 1)
+    }
+    return uid
+  }
+
+  const createPost = async () => {
+    if(confirm("새 게시물을 포스트 하시겠습니까?")){
+      let uid = getRandomUid();
+      const isOver = await API.graphql({
+        query: getPost,
+        variables: {
+          id: uid
+        }
+      }) as {data:{getPost:IPost | null}};
+      if(isOver.data.getPost === null){
+        const {data} = await API.graphql({
+          query: createPostQuery,
+          variables: {
+            input:{
+              id:uid,
+              title,
+              descript,
+              img:"https://i.ibb.co/60S7pbD/page1.png"
+            }
+          }
+        }) as {data:{createPost:IPost | null}};
+        if (data.createPost === null) window.alert("포스트에 실패했습니다.");
+        else window.location.reload();
+      }else{
+        alert("오류! 다시 시도해주세요.");
+      }
+    }
+  }
+
+  return (
+    <Container>
+      <h1>Create</h1>
+      <input value={title} onChange={(e)=>{setTitle(e.target.value)}} placeholder="제목을 작성하세요." type="text" />
+      <input value={descript} onChange={(e)=>{setDescript(e.target.value)}} placeholder="설명을 작성하세요." type="text" />
+      <CreatePostBtn onClick={createPost}>새 글 생성</CreatePostBtn>
+      <br/>
+      <br/>
+      <h1>List</h1>
+      {
+        posts.map((post: IPost, key) => (
+          <Link href={`/ssg/${post.id}`} key={key}><a>{post.title}</a></Link>
+        ))
+      }
+    </Container>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const SSR = withSSRContext();
+  const { data } = await SSR.API.graphql({ query: listPosts });
+  return {
+    props: {
+      posts: data.listPosts.items
+    },
+    revalidate: 60,
+  }
+}
+
+const Container = styled.div`
+  width:100vw;
+  min-height:100vh;
+  a{
+    display:block;
+    color:white;
+    font-size: 16px;
+  }
+  h1{
+    font-size: 18px;
+  }
+  input{
+    font-size: 14px;
+    display:block;
+    padding: 6px 8px;
+    margin-top: 6px;
+    border: 1px solid rgba(255,255,255,0.6);
+  }
+`
+const CreatePostBtn = styled.button`
+  margin: 16px 2px;
+  padding: 4px 8px;
+  border-radius: 2px;
+  background-color: rgba(255,255,255,1);
+  color: #121212;
+`
+
+export default Home
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface IPost {
+  readonly createdAt: String
+  readonly descript: String
+  readonly img: String
+  readonly title: String
+  readonly updatedAt: String
+  readonly id: String
+}
